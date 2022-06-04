0834c794caf3b7b188b3e4ae60e9adf13e477d51
byell-spec <gabryel.lymabiel@gmail.com>
2022-06-04 17:38:49 -0300

    second day

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 
 import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,5 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const database = firebase.database();
+
+export { firebase, auth, database };
